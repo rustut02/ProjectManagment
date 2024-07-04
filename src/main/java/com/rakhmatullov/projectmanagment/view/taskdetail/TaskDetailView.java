191747e2caa185a35d6b4d2e2d25081a255115ed
Rustam Rakhmatullov <rahmatullovri@gmail.com>
2024-07-04 15:24:21 +0400

    Task view realization

---
+++ src/main/java/com/rakhmatullov/projectmanagment/view/taskdetail/TaskDetailView.java
@@ -0,0 +1,14 @@
+package com.rakhmatullov.projectmanagment.view.taskdetail;
+
+
+import com.rakhmatullov.projectmanagment.entity.Task;
+import com.rakhmatullov.projectmanagment.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "tasks/:id", layout = MainView.class)
+@ViewController("Task_.detail")
+@ViewDescriptor("task-detail-view.xml")
+@EditedEntityContainer("taskDc")
+public class TaskDetailView extends StandardDetailView<Task> {
+}
